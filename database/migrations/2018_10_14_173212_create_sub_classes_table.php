ff16fd8a1cd5b53db0806e68d7da5bbe6072538c
Kristof Van Aken <vanakenk@gmail.com>
2018-10-14 19:53:52 +0200

    Race, class and background migrations & config file created

---
+++ database/migrations/2018_10_14_173212_create_sub_classes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubClassesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sub_classes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('name');
+            $table->unsignedInteger('class_id');
+            $table->foreign('class_id')->references('id')->on('classes');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sub_classes');
+    }
+}
